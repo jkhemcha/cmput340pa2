9cdfeed8d1706f797e70b38e470c2124634318c6
Jensen Khemchandani <jensenkhem@Jensens-MacBook-Pro.local>
2023-03-06 16:40:58 -0700

    Update

--- func_fit.m
+++ func_fit.m
@@ -2,10 +2,10 @@ function [coefficient_vector] = func_fit(X,Y,type,basis,parameters)
     % Input
     % X - input data (t)
     % Y - output data (y)
-    % type - ("interpolation", "approximation")
+    % type - ("interpolate", "approximate")
     %    "interpolation" - Generate interpolating polynomial for data
     %    "approximation" - Generate approximate polynomial for data
-    % basis - ("newton", "monomial")
+    % basis - ("poly", "trig")
     %    "poly" - Newton polynomial interpolation
     %    "trig" - Trig interpolation
     % parameters - Number of parameters to approximate when 'approximate' is selected
@@ -13,32 +13,40 @@ function [coefficient_vector] = func_fit(X,Y,type,basis,parameters)
     % Output
     % [coefficient_vector] vector of coefficients for approximate or interpolating polynomial
     
-    [~, input_size] = size(X)
-    
+    [~, input_size] = size(X);
+
     % Handle both poly and trig interpolation
-    if type == "interpolation"
+    if type == "interpolate"
         % Poly
         if basis == "poly"
-            basis_mat = ones(input_size, 1)
-            for i = 1:input_size - 1
-                col = []
-                for k = 1:input_size
-                    col = [col, (X(k) - X(i)) * basis_mat(k,i)]
-                end
-                basis_mat = [basis_mat, col']
+            basis_mat = [];
+            for i = 1:input_size
+                basis_mat = [basis_mat, X'.^(i-1)];
             end
-        
+            %basis_mat = ones(input_size, 1)
+            %for i = 1:input_size - 1
+            %    col = []
+            %    for k = 1:input_size
+            %        col = [col, (X(k) - X(i)) * basis_mat(k,i)]
+            %    end
+            %    basis_mat = [basis_mat, col']
+            %end
+            coefficient_vector = (basis_mat \ Y');
         % Trig
         elseif basis == "trig"
             % TODO
         end
-        coefficient_vector = (basis_mat \ Y');
     
     % Handle both poly and trig approximation
-    elseif type == "approximation"
+    elseif type == "approximate"
         % Poly
         if basis == "poly"
-            % TODO
+            basis_mat = [];
+            for i = 1:parameters
+                basis_mat = [basis_mat, X'.^(i-1)];
+            end
+            % Solve normal equations
+            coefficient_vector = (basis_mat' * basis_mat) \ (basis_mat' * Y');
         elseif basis == "trig"
             % TODO
         end
